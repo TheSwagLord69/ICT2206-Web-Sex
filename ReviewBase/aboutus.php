bb617be2ca358bcb4ae55bf21fc9014ac8026393
Hentai Salesman 69 <hentaisalesman69@protonmail.com>
2021-06-08 14:37:28 +0800

    Update aboutus.php

--- ReviewBase/aboutus.php
+++ ReviewBase/aboutus.php
@@ -25,7 +25,7 @@
 			<div class="container">
 				<div class="row">
 					<aside class="col-md-10">
-						<!-- ============================ COMPONENT INDEX  ================================= -->
+						<!-- ============================ COMPONENT ABOUTUS  ================================= -->
 						<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 						  <div class="col-md-5 p-lg-5 mx-auto my-5">
 							<h1 class="display-4 font-weight-normal">About us</h1>
@@ -35,7 +35,7 @@
 						  <div class="product-device box-shadow d-none d-md-block"></div>
 						  <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
 						</div>
-						<!-- ============================ COMPONENT INDEX END.// ================================= -->
+						<!-- ============================ COMPONENT ABOUTUS END.// ================================= -->
 					</aside>
 				</div>
 			</div>
