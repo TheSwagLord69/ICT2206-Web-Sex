67a3d763c07491a1e0efeb4e7838f72c3d754127
Hentai Salesman 69 <hentaisalesman69@protonmail.com>
2021-06-08 14:36:38 +0800

    Update logout.php

--- ReviewBase/logout.php
+++ ReviewBase/logout.php
@@ -46,7 +46,7 @@ session_destroy();
 			<div class="container">
 				<div class="row">
 					<aside class="col-md-10">
-						<!-- ============================ COMPONENT INDEX  ================================= -->
+						<!-- ============================ COMPONENT LOGGED OUT  ================================= -->
 						<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 						  <div class="col-md-5 p-lg-5 mx-auto my-5">
 							<h1 class="display-4 font-weight-normal">You have logged out</h1>
@@ -54,7 +54,7 @@ session_destroy();
 						  <div class="product-device box-shadow d-none d-md-block"></div>
 						  <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
 						</div>
-						<!-- ============================ COMPONENT INDEX END.// ================================= -->
+						<!-- ============================ COMPONENT LOGGED OUT END.// ================================= -->
 					</aside>
 				</div>
 			</div>
