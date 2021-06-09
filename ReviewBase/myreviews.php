8800f4d7fc7ef07e8b44f2dde0b47db163fce539
Hentai Salesman 69 <hentaisalesman69@protonmail.com>
2021-06-09 19:27:16 +0800

    Update myreviews.php

--- ReviewBase/myreviews.php
+++ ReviewBase/myreviews.php
@@ -42,13 +42,30 @@
 									$userid = $row["r_user_id"];
 									$productid = $row["r_product_id"];
 									$userreview = $row["user_review"];
-									echo 
-										'<br/><tr>
-											<td>reviewid: '.$reviewid.'</td>
-											<td>userid: '.$userid.'</td>
-											<td>productid: '.$productid.'</td>
-											<td>userreview: '.$userreview.'</td>
-										</tr><br/>';
+										
+										$sql1 = " SELECT `product_name` FROM `product_data` WHERE `product_id` = '$productid' ";
+										if ($result1 = $conn->query($sql1)) {
+											while ($row = $result1->fetch_assoc()) {
+												$productname = $row["product_name"];
+											}
+										}
+										
+									echo
+										'
+										<div class="card p-3 mt-2">
+											<div class="d-flex justify-content-between align-items-center">
+												<div class="user d-flex flex-row align-items-center">
+													<h3><span><small class="font-weight-bold text-primary" style="color: #53565B;">'.$productname.'</small></h3>
+												</div> 
+												<div class="user d-flex flex-row align-items-center" style="width:500px;"> 
+													<small class="font-weight-bold">'.$userreview.'</small></span> 
+												</div> 
+											</div>
+											<div class="action d-flex justify-content-between mt-2 align-items-center">
+												<div class="icons align-items-center"> <i class="fa fa-star text-warning"></i> <i class="fa fa-check-circle-o check-icon"></i> </div>
+											</div>
+										</div>
+										';
 								}
 							}
 							?>
