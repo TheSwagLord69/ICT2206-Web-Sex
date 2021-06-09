36c7445607fb05e6cbb2aa6f3d0f670403a89f2e
Hentai Salesman 69 <hentaisalesman69@protonmail.com>
2021-06-09 20:32:47 +0800

    Update navbar.php

--- ReviewBase/navbar.php
+++ ReviewBase/navbar.php
@@ -8,7 +8,7 @@
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 	  </button>
-	  <a class="navbar-brand" href="../Atlas/index.html">ICT2206 ReviewBase</a>
+	  <a class="navbar-brand" href="index.html">ICT2206 ReviewBase</a>
 	</div>
 
 	<!-- Collect the nav links, forms, and other content for toggling -->
